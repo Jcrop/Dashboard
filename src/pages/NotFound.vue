da7c92a77456caae1e3859f5239815a3b17646cc
Jcrop <jiom147147@gmail.com>
2022-08-12 10:57:02 -0500

    ajuste de pagina 404 not found

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,7 +1,12 @@
 <template>
-	<h1>
-		404
-	</h1>
+  <div class="not-found">
+  	<h1>
+  		4<span>0</span>4
+  	</h1>
+    <router-link class="btn" :to="{name: 'home'}">
+      Ir al Inicio
+    </router-link>
+  </div>
 </template>
 
 <script>
@@ -16,3 +21,25 @@ export default {
   }
 }
 </script>
+<style>
+  .not-found {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+  }
+
+  .not-found h1{
+    color: #0054FE;
+    margin: 0 0 30px;
+    font-size: 150px;
+    line-height: 1;
+    font-weight: 800;
+  }
+
+  .not-found h1 span {
+    opacity: 0.5;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
+    name: "home",
     redirect: '/login',
     component: () => import('@/views/MainLayout'),
     children: [{
